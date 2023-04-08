6b9269f1858cf321a990f4a33041a310a4df7d9f
NobitaZ <doanquytb@gmail.com>
2023-04-08 21:57:30 +0700

    re format code

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
@@ -4,7 +4,11 @@ import com.programmingtechie.inventoryservice.dto.InventoryResponse;
 import com.programmingtechie.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
